153e6c5615420199b8f6242d86ca36911c2c14c2
nuffer <nuffer@magicleap.com>
2020-04-03 23:54:32 +0200

    add dist

---
+++ dist/precache-manifest.a66903991dc7d42ed7710f16897cfd64.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ecbc67d2025d4e045a",
+    "url": "/css/app.8051ae6b.css"
+  },
+  {
+    "revision": "19ecbc67d2025d4e045a",
+    "url": "/js/app.1975cb47.js"
+  },
+  {
+    "revision": "9345e251226618f02b08",
+    "url": "/css/chunk-vendors.c99126ea.css"
+  },
+  {
+    "revision": "9345e251226618f02b08",
+    "url": "/js/chunk-vendors.9297188d.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "f62bf1a0ac87b907d8f7a58ebce7fed4",
+    "url": "/img/nucleo-icons.f62bf1a0.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4995e220570028a4a3d026062c93ccb8",
+    "url": "/img/fontawesome-webfont.4995e220.svg"
+  },
+  {
+    "revision": "e37ff18d9dec6fb17d566a93f6cb1f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064a17e2a5198d349d11e631650d92ed",
+    "url": "/img/brand/logo_alpha.png"
+  },
+  {
+    "revision": "a440eb31a022977353d0797b54c48a12",
+    "url": "/img/brand/logo_blue.png"
+  },
+  {
+    "revision": "e84668e1f3e02a2ffef7987a7c75d10c",
+    "url": "/img/brand/logo_blue_app.png"
+  },
+  {
+    "revision": "9fdfebf9b6d2b938c93b6609c52628be",
+    "url": "/img/brand/logo_white_app.png"
+  },
+  {
+    "revision": "98a95694a90c04f3d2f9e2902361394a",
+    "url": "/img/brand/logo_white.png"
+  },
+  {
+    "revision": "8b1a64df4cf428019119623de32597f0",
+    "url": "/pdf/press/swiss_press_release_2avril.pdf"
+  },
+  {
+    "revision": "58ce5483697057c627e1a03e64fed270",
+    "url": "/pdf/press/swiss_24heures.pdf"
+  }
+];
